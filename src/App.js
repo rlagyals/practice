caf8bb25b91f98c44c6c6c2e81bedb24470ea393
suhwan kim <playground99@naver.com>
2024-07-28 00:38:30 +0900

    피드백 추가

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Info from "./component/Info";
 import Hobby from "./component/Hobby";
 
 function App() {
-
+  //여기서 모든 상태 관리하고 각 컴포넌트에 props로 넘겨주는 방식도 한번 구현해보십쇼
+  //지금은 각 컴포넌트에서 상태를 관리하고 있음
+  
   return (
     <>
       <div className="App">
         <Header title="본인 소개 페이지" />
         <Info/>
-        <Hobby />
+        <Hobby/>
       </div>
     </>
   );
